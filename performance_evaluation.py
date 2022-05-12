8ea125a8b7db4a478b7d469ae34a52e9e2442a07
Blarc <jakob.malezic@gmail.com>
2022-05-12 19:29:27 +0200

    [feat] Update performance evaluation to be more descriptive.

--- performance_evaluation.py
+++ performance_evaluation.py
@@ -6,40 +6,38 @@ import numpy as np
 
 from tools.sequence_utils import VOTSequence
 from tools.sequence_utils import read_results
-import matplotlib.pyplot as plt
 
 
 def estimate_thresholds(scores, n):
-    
     scores_vector = np.sort(np.concatenate([np.array(s_) for s_ in scores], axis=0), axis=0)
     step = int(math.floor(scores_vector.shape[0] / (n - 1)))
 
     thresholds = [scores_vector[(i + 1) * step][0] for i in range(n - 2)]
     thresholds.insert(0, float('-inf'))
     thresholds.insert(len(thresholds), float('inf'))
-    
+
     return thresholds[::-1]
 
-def calculate_pr_re_f(sequences, overlaps, scores, thresholds):
 
+def calculate_pr_re_f(sequences, overlaps, scores, thresholds):
     pr = len(thresholds) * [float(0)]
     re = len(thresholds) * [float(0)]
 
     for i, sequence in enumerate(sequences):
-        
+
         seq_overlaps = np.array(overlaps[i]).flatten()
         seq_scores = np.array(scores[i]).flatten()
         visible = np.array(sequence.visible_frames()).flatten()
 
         n_visible = np.sum(visible)
-        
+
         seq_pr = len(thresholds) * [float(0)]
         seq_re = len(thresholds) * [float(0)]
 
         for ti, thr in enumerate(thresholds):
 
             selector = seq_scores > thr
-            
+
             if not any(selector):
                 seq_pr[ti] = 1
                 seq_re[ti] = 0
@@ -55,15 +53,15 @@ def calculate_pr_re_f(sequences, overlaps, scores, thresholds):
     f = [(2 * pr_ * re_) / (pr_ + re_) for pr_, re_ in zip(pr, re)]
 
     max_index = np.argmax(np.array(f))
-    
+
     pr_score = pr[max_index]
     re_score = re[max_index]
     f_score = f[max_index]
 
     return pr_score, re_score, f_score
 
-def evaluate_performance(dataset_path, results_dir):
-    
+
+def evaluate_performance(dataset_path, results_dir, latex=False):
     sequences = []
     with open(os.path.join(dataset_path, 'list.txt'), 'r') as f:
         for line in f.readlines():
@@ -72,11 +70,11 @@ def evaluate_performance(dataset_path, results_dir):
     scores_all = []
     overlaps_all = []
 
-    dataset = []
+    datasets = []
     for sequence_name in sequences:
-        
+
         sequence = VOTSequence(dataset_path, sequence_name)
-        dataset.append(sequence)
+        datasets.append(sequence)
 
         bboxes_path = os.path.join(results_dir, '%s_bboxes.txt' % sequence_name)
         scores_path = os.path.join(results_dir, '%s_scores.txt' % sequence_name)
@@ -89,26 +87,36 @@ def evaluate_performance(dataset_path, results_dir):
             exit(-1)
 
         overlaps = [sequence.overlap(bb, gt) for bb, gt in zip(bboxes, sequence.gt)]
-        
+
         scores_all.append(scores)
         overlaps_all.append(overlaps)
 
-    thresholds = estimate_thresholds(scores_all, 100)
+        pr, re, f = calculate_pr_re_f([sequence], [overlaps], [scores], estimate_thresholds([scores], 100))
+        if latex:
+            print(f'{sequence_name} & {pr} & {re} & {f} \\\\\n\\hline')
+        else:
+            print(f'----------{sequence_name}-----------')
+            print('Precision:', pr)
+            print('Recall:', re)
+            print('F-score:', f)
 
-    pr, re, f = calculate_pr_re_f(dataset, overlaps_all, scores_all, thresholds)
+    pr, re, f = calculate_pr_re_f(datasets, overlaps_all, scores_all, estimate_thresholds(scores_all, 100))
 
-    print('--------------------------')
-    print('Precision:', pr)
-    print('Recall:', re)
-    print('F-score:', f)
-    print('--------------------------')
+    if latex:
+        print(f'all & {pr} & {re} & {f} \\\\\n\\hline')
+    else:
+        print('----------all-------------')
+        print('Precision:', pr)
+        print('Recall:', re)
+        print('F-score:', f)
 
 
-parser = argparse.ArgumentParser(description='Longe-term Tracking Performance Evaluation Script')
+parser = argparse.ArgumentParser(description='Long-Term Tracking Performance Evaluation Script')
 
 parser.add_argument("--dataset", help="Path to the dataset", required=True, action='store')
 parser.add_argument("--results_dir", help="Path to the directory to store the results", required=True, action='store')
+parser.add_argument("--latex", help="Latex", required=False, action='store_true')
 
 args = parser.parse_args()
 
-evaluate_performance(args.dataset, args.results_dir)
+evaluate_performance(args.dataset, args.results_dir, args.latex)
